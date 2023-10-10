611252723f62cb76c1c35a67efee009738a1a904
j0se-2501 <jolufer23@gmail.com>
2023-10-10 20:52:45 +0200

    segunda activdad hecha

--- androidStudio/app/src/main/java/com/example/mediaasignaturas/MainActivity.java
+++ androidStudio/app/src/main/java/com/example/mediaasignaturas/MainActivity.java
@@ -2,13 +2,35 @@ package com.example.mediaasignaturas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button evaluarBoton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        evaluarBoton = findViewById(R.id.button);
+
+        evaluarBoton.setOnClickListener(new View.OnClickListener (){
+            public void onClick(View view){
+
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+
+    protected double calcularMedia(double asig1, double asig2, double asig3){
+        double media = (asig1+asig2+asig3)/3;
+        return media;
     }
 }
