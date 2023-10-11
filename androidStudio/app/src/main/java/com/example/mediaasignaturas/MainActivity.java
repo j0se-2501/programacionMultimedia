086d1446d2223ed58163cbb7293a748c14f14cbd
j0se-2501 <jolufer23@gmail.com>
2023-10-11 16:58:15 +0200

    apk media asignaturas terminada

--- androidStudio/app/src/main/java/com/example/mediaasignaturas/MainActivity.java
+++ androidStudio/app/src/main/java/com/example/mediaasignaturas/MainActivity.java
@@ -1,16 +1,18 @@
 package com.example.mediaasignaturas;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button evaluarBoton;
-
+    protected double asig1, asig2, asig3, media;
+    private TextView text1, text2, text3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +20,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         evaluarBoton = findViewById(R.id.button);
+        text1 = findViewById(R.id.editTextText);
+        text2 = findViewById(R.id.editTextText2);
+        text3 = findViewById(R.id.editTextText3);
 
         evaluarBoton.setOnClickListener(new View.OnClickListener (){
             public void onClick(View view){
 
+                asig1 = Double.parseDouble(text1.getText().toString());
+                asig2 = Double.parseDouble(text2.getText().toString());
+                asig3 = Double.parseDouble(text3.getText().toString());
+
+                media = calcularMedia(asig1, asig2, asig3);
+
                 Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                intent.putExtra("media", media);
                 startActivity(intent);
 
             }
--- androidStudio/app/src/main/java/com/example/mediaasignaturas/MainActivity2.java
+++ androidStudio/app/src/main/java/com/example/mediaasignaturas/MainActivity2.java
@@ -1,14 +1,31 @@
 package com.example.mediaasignaturas;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.text.DecimalFormat;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    private TextView text;
+    private double media;
+    private static final DecimalFormat df = new DecimalFormat("0.00");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        Intent intent = getIntent();
+        media = intent.getExtras().getDouble("media");
+        text = findViewById(R.id.textView2);
+
+        text.setText("Media: \n"+String.valueOf(df.format(media)));
+        if (media>=5.0) Toast.makeText (this, "Aprobado", Toast.LENGTH_LONG).show();
+        else Toast.makeText (this, "Suspenso", Toast.LENGTH_LONG).show();
     }
 }
